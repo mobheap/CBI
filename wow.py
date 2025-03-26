463855e6652679401439d2ff83758a3d66f04e81
mobheap <abbassipro27@gmail.com>
2025-03-26 11:52:00 +0000

    day 3

--- wow.py
+++ wow.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 with open('scraped_page.html', 'r', encoding='utf-8') as file:
     html = file.read()
@@ -13,11 +14,9 @@ with open('scraped_page.html', 'r', encoding='utf-8') as file:
 #     print('Failed')
 
 
-
 soup = BeautifulSoup(html, 'html.parser')
 
 products = soup.find_all('div', class_="x-ordered-inputs-initialized")
-# print(len(products))
 
 names = []
 ratings = []
@@ -25,28 +24,32 @@ nums_ratings = []
 
 for product in products:
     
-    useful = product.find('div', class_="product-card__info")
-    # if useful: print('ok')
-    name_container = useful.find('div', class_="product-card__product-name")
-    if name_container:
-        name = name_container.find('div').text
-        names.append(name)
+    try:    
+        useful = product.find('div', class_="product-card__info")
+        # if useful: print('ok')
+        name_container = useful.find('div', class_="product-card__product-name")
+        if name_container:
+            name = name_container.find('div').text.strip()
+            names.append(name)
+        
+        criticism = useful.find('div', class_="d-f ai-c fw-w")
+        rating = criticism.find('span', class_="fw-semibold").text.strip()
+        ratings.append(rating)
+        
+        num_ratings = criticism.find('span', class_='pl-4th').text.strip()
+        num_ratings = re.search(r"\(([\d,]+)\)", num_ratings).group(1).replace(',', '')
+        nums_ratings.append(num_ratings)
     
-    criticism = useful.find('div', class_="d-f ai-c fw-w")
-    rating = criticism.find('span', class_="fw-semibold").text
-    ratings.append(rating)
-    
-    num_ratings = criticism.find('span', class_='pl-4th').text.strip()
-    num_ratings = re.search(r"\(([\d,]+)\)", num_ratings).group(1).replace(',', '')
-    print(num_ratings)
-    nums_ratings.append(num_ratings)
-    
-# #     description_section = product.find('div', class_='product-card_content__cP10h')
-# #     description = description_section.find('span').text
-# #     descriptions.append(description)
-    
-# #     products_dict = {'Name': names, 'Rating-5': ratings, 'Number of reviews': nums_ratings, 'Description': descriptions}
-# #     products_df = pd.DataFrame(products_dict)
+    except Exception as e:
+        print(f'Error processing a product: {e}')
+        continue
 
-# # products_df.to_csv('APM tools Gartner.csv', index=False)
-# file.close()
+products_dict = {
+    'Name': names,
+    'Rating-5': ratings,
+    'Number of reviews': nums_ratings
+    }
+
+products_df = pd.DataFrame(products_dict)
+
+products_df.to_csv('APM tools G2.csv', index=False)
